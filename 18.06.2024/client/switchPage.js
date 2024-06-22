9f1fdce570c2af360dca6e5f82d600efbc6414b9
PML30\cgsg220 <aafolssad@gmail.com>
2024-06-22 17:01:04 +0300

    rrj ready

---
+++ 18.06.2024/client/switchPage.js
@@ -0,0 +1,28 @@
+import { currentArt } from "./index.js";
+
+export function switchPage(event, pageName) {
+  let tabContent;
+
+  if (currentArt == pageName) {
+    return;
+  }
+  alert($("#buy_miku_cow_art").attr("isBuying"));
+
+  $("#" + currentArt).slideUp(1000);
+  setTimeout(() => {
+    $("main").css(
+      "background-color",
+      $("#" + event.currentTarget.id).css("background-color")
+    );
+    $("aside").css(
+      "background-color",
+      $("#" + event.currentTarget.id).css("background-color")
+    );
+
+  }, 1);
+
+  $("#" + pageName).slideToggle(1000);
+  event.currentTarget.className += " active";
+  document.getElementById(pageName).style.display = "grid";
+  currentArt = pageName;
+}
